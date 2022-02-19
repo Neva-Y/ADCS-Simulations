e0d3b1be4af08d8a141978789d9d7821def16e11
Goh Xing Yang <69152768+Neva-Y@users.noreply.github.com>
2022-02-19 16:06:45 +1100

    Added rotational dynamics of CubeSAT in orbit

--- TIB.m
+++ TIB.m
@@ -11,3 +11,7 @@ out = [ct*cs,sp*st*cs-cp*ss,cp*st*cs+sp*ss;
     ct*ss,sp*st*ss+cp*cs,cp*st*ss-sp*cs;
     -st,sp*ct,cp*ct];
 end
+
+% Copyright - Carlos Montalvo 2015
+% You may freely distribute this file but please keep my name in here
+% as the original owner
